efe811d1fcba114c35295283e6d82a5ffcecfecb
Jonatas Gonzaga <33657806+Jonatasgonzaga@users.noreply.github.com>
2018-09-11 16:46:22 -0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace ConsoleApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sobreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.créditosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem,
+            this.sobreToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(495, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sairToolStripMenuItem});
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(73, 24);
+            this.arquivoToolStripMenuItem.Text = "Arquivo";
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.sairToolStripMenuItem.Text = "Sair";
+            // 
+            // sobreToolStripMenuItem
+            // 
+            this.sobreToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.créditosToolStripMenuItem});
+            this.sobreToolStripMenuItem.Name = "sobreToolStripMenuItem";
+            this.sobreToolStripMenuItem.Size = new System.Drawing.Size(60, 24);
+            this.sobreToolStripMenuItem.Text = "Sobre";
+            // 
+            // créditosToolStripMenuItem
+            // 
+            this.créditosToolStripMenuItem.Name = "créditosToolStripMenuItem";
+            this.créditosToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.créditosToolStripMenuItem.Text = "Créditos";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(189, 288);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 49);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(125, 217);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(233, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(136, 78);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(263, 122);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sobreToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem créditosToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading; 
+
+namespace Conversor
+{
+    class Program
+    {
+
+        public decimal ConversorBinFracionado(int contagem, decimal fracionado, List<object> bdfrac)
+        {
+            if (bdfrac.Count > 1)
+            {
+
+                if (contagem + 1 == bdfrac.Count)
+                {
+                    fracionado = Convert.ToDecimal(bdfrac[contagem]);
+
+                    return ConversorBinFracionado(contagem - 1, fracionado, bdfrac);
+                }
+
+                else if (contagem >= 0)
+                {
+
+                    fracionado = Convert.ToDecimal(bdfrac[contagem]) + 2 * fracionado;
+
+                    return ConversorBinFracionado(contagem - 1, fracionado, bdfrac);
+                }
+
+            }
+
+            else fracionado = Convert.ToDecimal(bdfrac[contagem]);
+            
+            return fracionado / (Decimal)Math.Pow(2, bdfrac.Count);
+        }
+
+        public Decimal ConversorBinInteiro(int contagem,decimal inteiro, List<object> bdint)
+        {
+            if (bdint.Count > 1)
+            {
+
+                if (contagem + 1 == bdint.Count)
+                {
+                    inteiro = Convert.ToDecimal(bdint[contagem]);
+
+                    return ConversorBinInteiro(contagem - 1, inteiro, bdint);
+                }
+
+                else if (contagem >= 0)
+                {
+
+                    inteiro = Convert.ToDecimal(bdint[contagem]) + 2 * inteiro;
+
+                    return ConversorBinInteiro(contagem - 1, inteiro, bdint);
+                }
+            }
+
+            else inteiro = Convert.ToDecimal(bdint[contagem]);
+
+            return inteiro;
+            
+
+        }
+
+        public List<object> ConversorDecFracionado(decimal fracionado, List<object> bdfrac, int freio)
+        {
+            
+
+            if (fracionado > 0 & freio > 0)
+             {
+                fracionado = fracionado * 2;
+                if (fracionado >= 1) bdfrac.Add(1); else bdfrac.Add(0);
+                return ConversorDecFracionado(fracionado - Math.Truncate(fracionado), bdfrac, freio-1);
+            }
+
+            else
+            {
+                return bdfrac;
+            }
+        }
+        public List<object> ConversorDecInteiro(decimal inteiro, List<object> bdint)
+        {
+                if (inteiro >= 1)
+                {
+                
+                    if (inteiro % 2 == 0)
+                    {
+                    inteiro = Math.Truncate(inteiro / 2);
+                    bdint.Add(0);
+                    }
+                     else
+                     {
+                    inteiro = Math.Truncate(inteiro / 2);
+                    bdint.Add(1); }
+                    return ConversorDecInteiro(inteiro, bdint);
+                }
+
+                else if (inteiro == 0 & bdint.Count == 0 )
+                {
+                bdint.Add(0);
+                return bdint;
+                }
+
+                else 
+                {
+                bdint.Reverse();
+                return bdint;
+                }
+
+            
+        }
+
+
+        static void Main(string[] args)
+        {
+            Program programa = new Program();
+            int freio = 7;
+            decimal inteiro = 0, fracionado, resposta;
+            string escolha;
+            var bdint = new List<object>();
+            var bdfrac = new List<object>();
+            string binario;
+            
+            
+
+            Console.WriteLine("Conversor entre DEC e BIN");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("Escolha uma das opções a seguir...");
+            Thread.Sleep(1500);
+            Console.Clear();
+            Console.WriteLine("Tecle a letra 'a' para converter de DEC para BIN");
+            Console.WriteLine("Tecle a letra 'b' para converter de BIN para DEC");
+            escolha = Console.ReadLine();
+            Console.Clear();
+
+            switch (escolha)
+            {
+                case "a":
+
+                    Console.WriteLine("Digite um número decimal: ");
+                    inteiro = Convert.ToDecimal(Console.ReadLine());
+                    fracionado = inteiro - Math.Truncate(inteiro);
+                    programa.ConversorDecInteiro(Math.Truncate(inteiro), bdint);
+                    Console.Write("Resposta:  ");
+                    foreach (var element in bdint) { Console.Write(element); }
+                    
+                    if (fracionado > 0)
+                    {
+                        programa.ConversorDecFracionado(fracionado, bdfrac, freio); Console.Write(",");
+                        foreach (var element in bdfrac) { Console.Write(element); }
+                    }
+
+                    
+                    Console.WriteLine();
+                    Console.WriteLine("Tecle ENTER para encerrar");
+                    Console.ReadLine();
+                    
+                    break;
+
+                case "b":
+
+                    Console.WriteLine("Digite um número em binário: ");
+                    inteiro = Convert.ToDecimal(Console.ReadLine());
+                    fracionado = inteiro - Math.Truncate(inteiro);
+                    inteiro = Math.Truncate(inteiro - fracionado);
+
+                    binario = inteiro.ToString();
+                    
+
+                    while (binario.Length != 0)
+                    {
+                        if (binario.EndsWith("0"))
+                        {
+                            bdint.Add(0);
+                            binario = binario.Remove(binario.Length-1);
+                        }
+
+                        else if (binario.EndsWith("1"))
+                        {
+                            bdint.Add(1);
+                            binario = binario.Remove(binario.Length-1);
+                        }
+
+                    }
+
+                    
+                    binario = fracionado.ToString();
+
+                    while (!binario.Equals("0"))
+                    {
+                        if (binario.EndsWith("0"))
+                        {
+                            bdfrac.Add(0);
+                            binario = binario.Remove(binario.Length-1);
+                        }
+
+                        else if (binario.EndsWith("1"))
+                        {
+                            bdfrac.Add(1);
+                            binario = binario.Remove(binario.Length-1);
+                        }
+
+                        else if (binario.EndsWith("."))
+                        {
+                            
+                            binario = binario.Remove(binario.Length - 1);
+
+                        }
+                    }
+
+                    if (bdfrac.Count > 0)
+                        resposta = programa.ConversorBinInteiro(bdint.Count - 1, inteiro, bdint) + programa.ConversorBinFracionado(bdfrac.Count - 1, fracionado, bdfrac);
+                    else
+                        resposta = programa.ConversorBinInteiro(bdint.Count - 1, inteiro, bdint);
+
+                    Console.WriteLine("Resultado: " + resposta);
+
+                    Console.ReadLine();
+                    
+                    
+                    break;
+                        
+
+            }
+
+        }
+    }
+}
